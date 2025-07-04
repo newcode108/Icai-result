dad5f0278d4a2d3b28135a655b080b95abad461d
newcode108 <aayushalternate5421@gmail.com>
2025-07-04 20:04:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("resultForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const form = this;
+  form.style.opacity = "0.5";
+  form.querySelector("button").textContent = "Loading...";
+  setTimeout(() => {
+    form.classList.add("hidden");
+    document.getElementById("errorScreen").classList.remove("hidden");
+  }, 800);
+});
